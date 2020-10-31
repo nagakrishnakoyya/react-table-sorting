b349030b367d7ec53ec3ad31d01edae4e66767e6
soma sekhar novudu <somasekharnovudu@somas-MacBook-Pro.local>
2020-10-31 21:14:59 +0530

    table pagination

--- client/src/components/RegularTable.js
+++ client/src/components/RegularTable.js
@@ -7,15 +7,26 @@ import Pagination from './pagination/Pagination';
 function RegularTable() {
     const [countries, setCountries] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage, setItemsPerPage] = useState(10);
     const [searchKey, setSearchKey] = useState('');
-    const [filteredItems, setFilteredItems] = useState([]);
-    const [isFiltered, setIsFiltered] = useState(false);
-    const [filteredPageCount, setFilteredPageCount] = useState(0);
-    const countriesPageCount = Math.ceil(countries.length/itemsPerPage);
-    const finalResults = isFiltered ? filteredItems : countries;
-    const finalPageCount = isFiltered ? filteredPageCount : countriesPageCount;;
+    const [results, setResults] = useState(countries);
+    const [itemsPerPage, setItemsPerPage] = useState(10);
+    const [pageCount, setPageCount] = useState(Math.ceil(results.length / itemsPerPage));
+    const [tableMeta, updateTableMeta] = useState({
+        indexOfFirstpage: 0,
+        currentItems: [],
+        indexOfLastpage: 1
+    })
 
+    const updateResults = (resultsArr) => {
+        setResults(resultsArr);
+        setCurrentPage(1);
+        setPageCount(Math.ceil(resultsArr.length / itemsPerPage))
+    }
+
+    const updateItemsPerPage = (pageItemCount) => {
+        setItemsPerPage(pageItemCount);
+        setResults(results);
+    }
 
     useEffect(() => {
         const getCountries = () => {
@@ -23,8 +34,6 @@ function RegularTable() {
                 .then(res => res.json())
                 .then(data => {
                     setCountries(data);
-                    // let pageCount = Math.ceil(data.length / itemsPerPage);
-                    // setPageCount(pageCount);
                 })
                 .catch(error => console.log(error))
         }
@@ -32,11 +41,24 @@ function RegularTable() {
         logItems();
     }, []);
 
-    
-    // Get current page items
-    const indexOfLastpage = currentPage * itemsPerPage;
-    const indexOfFirstpage = indexOfLastpage - itemsPerPage;
-    const currentItems = finalResults.slice(indexOfFirstpage, indexOfFirstpage + itemsPerPage);
+    useEffect(() => {
+        console.log('into country update ')
+        updateResults(countries);
+    }, [countries.length])
+
+    useEffect(() => {
+        updateTableMeta(() => {
+            const indexOfLastPage = currentPage * itemsPerPage;
+            const indexOfFirstpage = indexOfLastPage - itemsPerPage;
+            const currentItems = results.slice(indexOfFirstpage, indexOfFirstpage + itemsPerPage);
+            return {
+                indexOfFirstpage,
+                indexOfLastPage,
+                currentItems
+            }
+        })
+        setPageCount(Math.ceil(results.length / itemsPerPage))
+    }, [results, itemsPerPage, currentPage])
 
     // paginateHandler
     const paginateHandler = (e, number) => {
@@ -53,48 +75,26 @@ function RegularTable() {
     // nextPageHandler
     const nextPageHandler = (e) => {
         e.preventDefault();
-        if (currentPage < finalPageCount) {
+        if (currentPage < pageCount) {
             setCurrentPage(currentPage + 1);
         }
     }
 
-    const setItemsAndPage=(selectedValue)=>{
-        setItemsPerPage(parseInt(selectedValue));
-        setCurrentPage(1);
-        // searchCountriesHandler(e);
-        console.log("selectedValue", selectedValue);
-
-    }
-
     const itemsPerPageHandler = (e) => {
         const selectedValue = e.target.value;
-        setItemsAndPage(selectedValue);
+        updateItemsPerPage(parseInt(selectedValue));
+        updateResults(results);
     }
 
     const searchCountriesHandler = (e) => {
-        debugger;
         const searchKeyword = e.target.value;
         setSearchKey(searchKeyword);
         let searchedItems = countries.filter(item => {
             return item.name.toLowerCase().match(searchKey.toLowerCase());
         })
-        setFilteredItems(searchedItems);
-        let pageCount = Math.ceil(searchedItems.length / itemsPerPage);
-        console.log("pageCount", pageCount);
-        setFilteredPageCount(pageCount);
-        if (searchKeyword !=="") {
-            setIsFiltered(true);
-        }
-        else {
-            setIsFiltered(false);
-        }
-        setItemsAndPage(itemsPerPage);
+        updateResults(searchedItems);
         console.log("searchKeyword", searchKeyword);
     }
-
-    useEffect(()=>{
-        searchCountriesHandler(e);
-    }, [itemsPerPage]);
 
 
     const numberFormat = (number) => {
@@ -103,21 +103,21 @@ function RegularTable() {
 
 
 
-  // console.log(test)
-  const logItems = () => {
-    // console.log("currentPage", currentPage);
-    // console.log("itemsPerPage", itemsPerPage);
-    // console.log("indexOfLastpage", indexOfLastpage);
-    // console.log("currentItems", currentItems);
-    // console.log("countries", countries[0]);
-    // console.log("countries", countries[countries.length-1]);
-    // console.log("pageCount", pageCount);
-}
+    // console.log(test)
+    const logItems = () => {
+        // console.log("currentPage", currentPage);
+        // console.log("itemsPerPage", itemsPerPage);
+        // console.log("indexOfLastpage", indexOfLastpage);
+        // console.log("currentItems", currentItems);
+        // console.log("countries", countries[0]);
+        // console.log("countries", countries[countries.length-1]);
+        // console.log("pageCount", pageCount);
+    }
 
     return (
         <div className="container-fluid">
             {
-                countries.length > 0 ? (
+                tableMeta.currentItems.length > 0 ? (
                     <>
                         {/* <h2>Regular Table</h2> */}
                         <div className="row">
@@ -147,9 +147,9 @@ function RegularTable() {
                                 </thead>
                                 <tbody>
                                     {
-                                        currentItems.map((country, index) => (
+                                        tableMeta.currentItems.map((country, index) => (
                                             <tr key={country.name}>
-                                                <td>{index + 1 + indexOfFirstpage}</td>
+                                                <td>{index + 1 + tableMeta.indexOfFirstpage}</td>
                                                 <td>{country.name}</td>
                                                 <td>{country.capital}</td>
                                                 <td>{country.currencies[0].name}</td>
@@ -165,7 +165,7 @@ function RegularTable() {
                             <Pagination
                                 // itemsPerPage={itemsPerPage}
                                 // totalCountries={countries.length}
-                                pageCount={finalPageCount}
+                                pageCount={pageCount}
                                 paginate={paginateHandler}
                                 prevPage={prevPageHandler}
                                 nextPage={nextPageHandler}
